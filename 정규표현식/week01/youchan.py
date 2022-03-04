9869d86b2aada4ea87df730f40cb44e0e0f13cce
U GO <67580960+u-go-ss@users.noreply.github.com>
2022-03-04 14:12:22 +0900

    Update youchan.py

--- 정규표현식/week01/youchan.py
+++ 정규표현식/week01/youchan.py
@@ -1,8 +1,15 @@
 # 1.
-Regex_Pattern = r'hackerrank'
+var Regex_Pattern = /hackerrank/;
 
 # 2. xxx.xxx.xxx.xxx
-regex_pattern = r"^.{3}(\..{3}){3}$"
+var Regex_Pattern = /^.{3}(\..{3}){3}$/g;
 
-# xxXxxXxxxx x는 숫자, X는 숫자가 아닌 문자
-Regex_Pattern = r"(\d\d\D){2}\d{4}"
+# 3. xxXxxXxxxx x는 숫자, X는 숫자가 아닌 문자
+var Regex_Pattern = /(\d\d\D){2}\d{4}/;
+
+# 4.  XXxXXxXX  x는 공백문자, X는 공백이 아닌 문자
+var Regex_Pattern = /(\S\S\s){2}\S\S/;
+
+# 5.
+
+# 6.
