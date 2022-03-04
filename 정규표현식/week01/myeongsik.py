ff16f404e52ef142e52e8ed57f8c6cc3aad5c974
ThingSik1997 <99626816+ThingSik1997@users.noreply.github.com>
2022-03-04 15:20:26 +0900

    Update myeongsik.py

--- 정규표현식/week01/myeongsik.py
+++ 정규표현식/week01/myeongsik.py
@@ -18,3 +18,21 @@
 2.Regex_Pattern = r'^[^/d][^a,e,i,o,u][^b,c,D,F][^\r,\n,\t,\f,\s][^A,E,I,O,U][^\.,\,]$'
 
 3.Regex_Pattern = r'^[a-z][1-9][^a-z][^A-Z][A-Z]'
+
+20220303
+1.Regex_Pattern = r'[\bI\b,\b..y\b]'
+
+2.Regex_Pattern = r'(ok){3}'
+
+3.Regex_Pattern = r'^(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[^ V,\#,\_][\w]'
+
+20220304
+1.Regex_Pattern = r'^[02468a-zA-Z]{40}[13579\s]{5}$'
+
+2.Regex_Pattern = r'^\d{1,2}[a-zA-Z]{3,}\.{0,3}$' (다시 풀기)
+
+3.Regex_Pattern = r'^\d{2,}[a-z]*[A-Z]*$'
+
+4.Regex_Pattern = r'^\d{1,}[A-Z]+[a-z]+$'
+
+5.Regex_Pattern = r'^[a-zA-Z]*s$'
